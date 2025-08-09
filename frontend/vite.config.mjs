279644a0d32fd17016a7abe396b0692b356cb958
Tanzin Rahman <aonkon16@gmail.com>
2025-08-09 22:38:21 +0900

    fix: vite configuration

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,14 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // FIX: Set the base path to your repository name
-  base: '/clear-cut-markets/', 
-  
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+export default defineConfig(({ command }) => {
+  return {
+    // FIX: Conditionally set the base path only for the build command
+    base: command === 'build' ? '/clear-cut-markets/' : '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    }
   }
 })
