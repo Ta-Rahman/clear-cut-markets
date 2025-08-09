4e1c780cc3d0c92cc6fadce6ade3ad1ef67060af
Tanzin Rahman <aonkon16@gmail.com>
2025-08-10 01:08:49 +0900

    fix: false imports removed

--- frontend/src/views/pages/Landing.vue
+++ frontend/src/views/pages/Landing.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-import FeaturesWidget from '@/components/landing/FeaturesWidget.vue';
-import FooterWidget from '@/components/landing/FooterWidget.vue';
-import HeroWidget from '@/components/landing/HeroWidget.vue';
-import HighlightsWidget from '@/components/landing/HighlightsWidget.vue';
-import PricingWidget from '@/components/landing/PricingWidget.vue';
-import TopbarWidget from '@/components/landing/TopbarWidget.vue';
 </script>
 
 <template>
